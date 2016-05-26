3a028371e563766249ff28b52a26163597b541c4
Bernard Wolff <bernard.wolff@tntp.org>
2016-05-26 10:52:34 -0400

    update assembly version and company

--- NSurveyGizmo/Properties/AssemblyInfo.cs
+++ NSurveyGizmo/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NSurveyGizmo")]
 [assembly: AssemblyDescription(".NET client for the Survey Gizmo REST API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Bernard Wolff")]
+[assembly: AssemblyCompany("TNTP")]
 [assembly: AssemblyProduct("NSurveyGizmo")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
