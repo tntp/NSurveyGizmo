2395ac45bbe8df3373ec03ffd084a8d1294eb367
Robert DeSimone <icenine457@gmail.com>
2017-02-16 12:40:22 -0500

    Revert "Add varname to SurveyQuestion model"
    This reverts commit 83e01114645683a798d9ce45b49cc3026d13656c.

--- NSurveyGizmo.Tests/IntegrationTests.cs
+++ NSurveyGizmo.Tests/IntegrationTests.cs
@@ -25,23 +25,16 @@ namespace NSurveyGizmo.Tests
 
             var questions = apiClient.GetQuestions(2687802).Where(q => q._type == "SurveyQuestion").ToList();
 
-            var gizmoQuestions = questions.Select(q =>
+            var gizmoQuestions = questions.Select(q => new 
             {
-                var qCode = q.varname.Trim();
-                if (string.IsNullOrEmpty(qCode))
-                {
-                    qCode = q.properties?.question_description?.English != null
+                ID = q.id.ToString(),
+                Question = q.title != null ? q.title.English + joinOptions(q.options) : "",
+                QCode =
+                    q.properties != null && q.properties.question_description != null &&
+                    q.properties.question_description.English != null
                         ? HtmlRegex.Replace(q.properties.question_description.English, string.Empty)
-                        : "";
-                }
-
-                return new
-                {
-                    ID = q.id.ToString(),
-                    Question = q.title != null ? q.title.English + joinOptions(q.options) : "",
-                    QCode = qCode,
-                    AnswerFormat = q._subtype ?? ""
-                };
+                        : "",
+                AnswerFormat = q._subtype ?? ""
             }).ToList();
 
 
