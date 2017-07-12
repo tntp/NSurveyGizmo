f250879057428e0cb160c7c1e74dad1ab0620af8
Nicole Parisis <nicole.parisis@tntp.org>
2017-07-12 16:32:25 -0400

    INRS-1420 fixed contact class

--- NSurveyGizmo/Models/Contact.cs
+++ NSurveyGizmo/Models/Contact.cs
@@ -7,18 +7,6 @@ namespace NSurveyGizmo.Models
     {
         [Key]
         public int id { get; set; }
-        public string emailAddress { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string organization { get; set; }
-        public string division { get; set; }
-        public string team { get; set; }
-        public string group { get; set; }
-        public string role { get; set; }
-        public string homephone { get; set; }
-        public string faxphone { get; set; }
-        public string businessphone { get; set; }
-        public string mailingaddress { get; set; }
         public string scustomfield5 { get; set; }
         public string scustomfield7 { get; set; }
         public string scustomfield8 { get; set; }
@@ -47,18 +35,18 @@ namespace NSurveyGizmo.Models
         public bool Equals(Contact contact)
         {
             return id             == contact.id
-                && emailAddress   == contact.emailAddress
-                && firstName      == contact.firstName
-                && lastName       == contact.lastName
-                && organization   == contact.organization
-                && division       == contact.division
-                && team           == contact.team
-                && group          == contact.group
-                && role           == contact.role
-                && homephone      == contact.homephone
-                && faxphone       == contact.faxphone
-                && businessphone  == contact.businessphone
-                && mailingaddress == contact.mailingaddress
+                && semailaddress  == contact.semailaddress
+                && sfirstname     == contact.sfirstname
+                && slastname      == contact.slastname
+                && sorganization  == contact.sorganization
+                && sdivision      == contact.sdivision
+                && steam          == contact.steam
+                && sgroup         == contact.sgroup
+                && srole          == contact.srole
+                && shomephone     == contact.shomephone
+                && sfaxphone      == contact.sfaxphone
+                && sbusinessphone == contact.sbusinessphone
+                && smailingaddress== contact.smailingaddress
                 && scustomfield5  == contact.scustomfield5
                 && scustomfield7  == contact.scustomfield7
                 && scustomfield8  == contact.scustomfield8
